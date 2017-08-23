de6009ffcff0a8e9d73087b30b28597334a1e78c
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-24 02:32:47 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,59 +1,49 @@
 import { JumpFm } from 'jumpfm-api'
-
 import { JumpDb } from './JumpDb'
 
 import * as fs from 'fs-extra'
 import * as fuzzy from 'fuzzy'
 
+export const load = (jumpFm: JumpFm) => {
+    const jumpDb = new JumpDb(
+        jumpFm.root,
+        jumpFm.settings.getNum('jumpMaxDbSize', 300),
+        jumpFm.settings.getNum('jumpDbSaveInterval', 5)
+    )
 
-class JumpDialog {
-    label = 'Jump'
-    jumpFm: JumpFm
-    jumpDb
 
-    constructor(jumpFm) {
-        this.jumpFm = jumpFm
-        const getNum = jumpFm.settings.getNum
+    jumpFm.panels.forEach(panel =>
+        panel.onCd(() => {
+            jumpDb.visit(panel.getUrl().path)
+        })
+    )
 
-        this.jumpDb = new JumpDb(
-            jumpFm.root,
-            getNum('jumpMaxDbSize', 300),
-            getNum('jumpDbSaveInterval', 5)
-        )
-    }
+    jumpFm.bind('jump', ['j'], () =>
+        jumpFm.dialog.open({
+            label: 'Jump'
+            , onAccept: (val: string, sug) => {
+                jumpFm.getPanelActive().cd(sug.value)
+            }
+            , suggest: (val: string) => {
+                console.log('suggesting')
+                const files = jumpDb.db.filter(file => fs.existsSync(file))
+                const pattern = val.replace(/\s/, '')
+                const options = { pre: '<b>', post: '</b>' }
 
-    onPanelCd = (url) => {
-        this.jumpDb.visit(url.path)
-    }
-
-    onChange = (val: string) => {
-        const files = this.jumpDb.db.filter(file => fs.existsSync(file))
-        const pattern = val.replace(/\s/, '')
-        const options = { pre: '<b>', post: '</b>' }
-
-        return fuzzy
-            .filter(pattern, files, options)
-            .sort((a, b) => (b.score - a.score) || (a.index - b.index))
-            .splice(0, 12)
-            .map(res => {
-                return {
-                    value: res.original as string,
-                    html: res.string
-                }
-            })
-    }
-
-    onAccept = (val: string, sug) => {
-        this.jumpFm.getActivePanel().cd(sug.value)
-    }
-}
-
-export const load = (jumpFm) => {
-    const jumpDialog = new JumpDialog(jumpFm)
-
-    jumpFm.panels.forEach(panel => panel.listen(jumpDialog))
-    jumpFm.bindKeys('jump', ['j'], () =>
-        jumpFm.dialog.open(jumpDialog)).filterMode([])
+                return fuzzy
+                    .filter(pattern, files, options)
+                    .sort((a, b) => (b.score - a.score) || (a.index - b.index))
+                    .splice(0, 12)
+                    .map(res => {
+                        return {
+                            value: res.original as string,
+                            html: res.string
+                        }
+                    })
+            }
+            , onOpen: input => input.select()
+        })
+    )
 }
 
 
